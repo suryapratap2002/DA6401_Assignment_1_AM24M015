6277222d44c0e023cb7109e297eff4869fa74ffd
Surya Pratap Singh <spsinghiitian2020@gmail.com>
2026-03-08 22:41:06 +0530

    Delete __init__.py

--- __init__.py
+++
@@ -1,8 +0,0 @@
-"""
-ann — Artificial Neural Network building blocks.
-"""
-from ann.activations        import get_activation, sigmoid, tanh, relu, softmax
-from ann.neural_layer       import NeuralLayer
-from ann.neural_network     import NeuralNetwork
-from ann.objective_functions import get_loss
-from ann.optimizers         import get_optimizer
